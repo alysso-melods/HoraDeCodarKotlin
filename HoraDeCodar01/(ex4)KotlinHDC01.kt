4dc914bbdac014f3e2afca3627239c98a9e8e20f
Alysson Melo <alyssonn.melo@gmail.com>
2024-09-09 20:52:30 -0300

    Add files via upload

---
+++ HoraDeCodar01/(ex4)KotlinHDC01.kt
@@ -0,0 +1,69 @@
+/*
+fun main() {
+
+    //Retângulo
+    println("Insira a base do Retangulo: ")
+    val base: Float = readln().toFloat()
+    println("Insira a altura do Retângulo: ")
+    val altura: Float = readln().toFloat()
+    val area: Float
+    area = base*altura
+    println("A araa total do Retângulo é: $area")
+
+    //Quadrado
+    println("Insira o lado do Quadrado: ")
+    val lado: Float = readln().toFloat()
+    val area: Float
+    area = lado*lado
+    println("A area total do Quadrado é: $area")
+
+    //Paralelograma
+    println("Insira a base do Paralelogramo: ")
+    val base: Float = readln().toFloat()
+    println("Insira a altura do Paralelogramo: ")
+    val altura: Float = readln().toFloat()
+    val area: Float
+    area = base*altura
+    println("Area total do Paralelograma é: $area")
+
+    //Losango
+    println("Insira a diagonal maior do Losango: ")
+    val diagonal_maior: Float = readln().toFloat()
+    println("Insira a diagonal menor do Losango: ")
+    val diagonal_menor: Float = readln().toFloat()
+    val area: Float
+    area = (diagonal_maior * diagonal_menor) / 2
+    println("A area total do Losango é: $area")
+
+    //Circulo
+    val numero_pi: Double = 3.14
+    println("Insira o raio do Circulo: ")
+    val raio: Double = readln().toDouble()
+    val area: Double
+    area = (numero_pi * raio)
+    println("A aréa total do Circulo é:$area")
+
+    //Trapézio
+    println("Insira a base maior do Trapézio: ")
+    val base_maior: Float = readln().toFloat()
+    println("Insira a base menor do Trapézio: ")
+    val base_menor: Float = readln().toFloat()
+    println("Insira a altura do Trapézioo: ")
+    val altura: Float = readln().toFloat()
+    val area: Float
+    area = (base_maior + base_menor)*altura / 2
+    println("A area total do trapézio: $area")
+
+    //Triângulo
+    println("Insira a base do Triângulo: ")
+    val base: Float = readln().toFloat()
+    println("Insira a altura do Triângulo: ")
+    val altura: Float = readln().toFloat()
+    val area: Float
+    area = (base*altura) / 2
+    println("A aréa total do Triângulo é:$area")
+
+    */
+
+}
+
